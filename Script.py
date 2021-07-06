5bb9089f482690e44344f599c2d8cd75836e8fda
Aman Mishra <49846395+amanrun@users.noreply.github.com>
2021-07-07 01:36:01 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import *
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("https://raw.githubusercontent.com/akhil14shukla/IME672A-Course-Project/master/hmeq.csv")
+
+sns.pairplot(df)
+
+df.isna().sum()
 
